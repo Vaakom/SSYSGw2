eaaeaafb690b94deccc24dd784c08903476030cb
Vaakom <systemplast@yandex.ru>
2017-01-17 02:43:56 +0300

    Migrate to angular-cli

--- app/app.routing.ts
+++
@@ -1,8 +0,0 @@
-import {Router, RouterModule} from '@angular/router';
-
-import {LoginFormComponent} from "./login/login-form.component";
-
-export const routing = RouterModule.forRoot([
-    {path: '', component: LoginFormComponent},
-    {path: '', component: LoginFormComponent}
-]);
