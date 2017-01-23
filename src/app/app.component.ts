9a077c542f4e247e3efea287c1a0fde96f4203b2
Vaakom <systemplast@yandex.ru>
2017-01-24 01:21:46 +0300

    Vertical menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: `        
-        <top-menu #topMenu></top-menu>
-
-        <div class="container-fluid">
-            <router-outlet (onSuccessLogin)="console.log('Cought it!');"></router-outlet>
-        </div>
-    `
+    templateUrl: 'app.component.html'
 })
 
 export class AppComponent {
